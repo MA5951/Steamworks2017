2eacc9cc166d9cfad93d92e3463d3fd49252ff3c
יאיר זיו < >
2017-02-06 13:41:14 +0200

    Added enum for xbox button numbers

--- src/org/usfirst/frc/team5951/robot/OI.java
+++ src/org/usfirst/frc/team5951/robot/OI.java
@@ -8,30 +8,51 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
-public class OI{
-	
+public class OI {
+
 	public static final Joystick k_DRIVER_JOYSTICK = new Joystick(0);
 	public static final XboxController k_SYSTEMS_DRIVER_STICK = new XboxController(1);
-	
-	public static final JoystickButton k_TOGGLE_SHIFTERS = new JoystickButton(k_DRIVER_JOYSTICK, ButtonNumbers.k_TRIGGER.getValue());
-	
-	public OI(){
-		
+
+	public static final JoystickButton k_TOGGLE_SHIFTERS = new JoystickButton(k_DRIVER_JOYSTICK, ButtonNumbers.TRIGGER.getValue());
+
+	public OI() {
+
 	}
-	
+
 }
 
-enum ButtonNumbers{
+enum ButtonNumbers {
+
+	// XBOX Buttons
+	X(1), 
+	A(2), 
+	B(3), 
+	Y(4), 
+	BUMPER_LEFT(5), 
+	BUMPER_RIGHT(7),
+	LB(5),
+	RB(7),
+	BACK(9), 
+	START(10), 
+	LEFT_STICK_BUTTON(11), 
+	RIGHT_STICK_BUTTON(12), 
+	XBOX_BUTTON(13),
 	
-	k_TRIGGER(1),
-	k_THUMB_BUTTON(2);
-	
+	//Logitech Extreme 3D Buttons
+	TRIGGER(1),
+	THUMB_BUTTON(2),
+	BOTTOM_LEFT(3),
+	BUTTOM_RIGHT(4),
+	TOP_LEFT(5),
+	TOP_RIGHT(6);
+
 	private int value;
-	ButtonNumbers(int value){
-		this.value = value;
-	}
-	
-	public int getValue(){
-		return this.value;
+
+	private ButtonNumbers(int value) {
+			this.value = value;
+		}
+
+	public int get() {
+		return value;
 	}
 }
